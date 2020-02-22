63f40472a22a8982b3ba1b0199489109fe345889
David Moates <david.m.moates@gmail.com>
2020-02-21 20:57:08 -0600

    error with login that is now fixed

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import axiosWithAuth from "../axiosWithAuth";
 
-const Login = () => {
-  // make a post request to retrieve a token from the api
-  // when you have handled the token, navigate to the BubblePage route
+const Login = props => {
+  const [login, setLogin] = useState({
+    username: "",
+    password: ""
+  });
+
+  const handleChange = e => {
+    setLogin({
+      ...login,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/api/login", login)
+      .then(res => {
+        console.log("Login Results", res);
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/bubbles");
+      })
+      .catch(err => {
+        console.error("Login Error", err.message);
+      });
+  };
+
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          placeholder="username"
+          value={login.name}
+          onChange
+          {...handleChange}
+        />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          value={login.name}
+          onChange
+          {...handleChange}
+        />
+        <button type="submit">Login</button>
+      </form>
     </>
   );
 };
